610e0b214c911f86ab91d2cef4ea7dee9d2910bb
Bray Madoué KAGONGBE <97260352+CyberBray235@users.noreply.github.com>
2026-06-23 09:17:46 +0000

    Validation

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/monportfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
